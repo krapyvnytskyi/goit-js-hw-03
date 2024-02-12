ce243b5daa766bdec77bf27ec3723a0abf96a9bc
krapyvnytskyi <krapev@gmail.com>
2024-02-12 11:24:41 +0200

    Upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,11 @@
-// function makeArray(firstArray, secondArray, maxLength) {
-//   const arr = firstArray.concat(secondArray);
-//   if (arr.length > maxLength) {
-//     return arr.slice(0, maxLength);
-//   } else {
-//     return arr;
-//   }
-// }
+function makeArray(firstArray, secondArray, maxLength) {
+  const arr = firstArray.concat(secondArray);
+  if (arr.length > maxLength) {
+    return arr.slice(0, maxLength);
+  } else {
+    return arr;
+  }
+}
 
 // console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
 // console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
